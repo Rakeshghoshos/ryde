c7856ef1f18e4a161be9be635e4147a2fa90ccff
Rakeshghoshos <rakeshghosh126@gmail.com>
2024-11-19 07:44:01 -0800

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const index = () => {
+  return (
+    <SafeAreaView className="flex justify-center items-center">
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+
+export default index;
